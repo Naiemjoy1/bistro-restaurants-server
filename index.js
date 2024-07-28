1304f90e43861632c6e6b6dfd65149cb8c414965
Naiem Hasan <naiemjoy1@gmail.com>
2024-07-28 21:49:34 +0600

    add to cart

--- index.js
+++ index.js
@@ -25,9 +25,30 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const menuCollection = client.db("bossDb").collection("menu");
+    const cartCollection = client.db("bossDb").collection("carts");
 
+    // menu api
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    // cart api
+    app.get("/carts", async (req, res) => {
+      const result = await cartCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
       res.send(result);
     });
 
