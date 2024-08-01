b96634c288362e215d8d0bb592af4ebcbe1bdd24
Naiem Hasan <naiemjoy1@gmail.com>
2024-08-01 21:57:11 +0600

    payment for ssl

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ const port = process.env.PORT || 3000;
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { default: axios } = require("axios");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ccm0dfs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +33,7 @@ async function run() {
     const menuCollection = client.db("bossDb").collection("menu");
     const cartCollection = client.db("bossDb").collection("carts");
     const paymentCollection = client.db("bossDb").collection("payments");
+    const sslCollection = client.db("bossDb").collection("ssl");
 
     // jwt api
     app.post("/jwt", async (req, res) => {
@@ -229,6 +232,7 @@ async function run() {
       const payment = req.body;
       const paymentResult = await paymentCollection.insertOne(payment);
       console.log("payments info", payment);
+
       // delete cart items
       const query = {
         _id: {
@@ -262,6 +266,120 @@ async function run() {
       });
 
       res.send({ paymentResult, deleteResult });
+    });
+
+    // ssl
+    app.post("/create-payment", async (req, res) => {
+      const paymentssl = req.body;
+      const trxId = new ObjectId().toString();
+      const initiateData = {
+        store_id: "progr666c8685d9799",
+        store_passwd: "progr666c8685d9799@ssl",
+        total_amount: paymentssl.amount,
+        currency: paymentssl.currency,
+        tran_id: trxId,
+        success_url: "http://localhost:3000/success-payment",
+        fail_url: "http://localhost:3000/fail",
+        cancel_url: "http://localhost:3000/cancle",
+        cus_name: paymentssl.name,
+        cus_email: paymentssl.email,
+        cus_add1: "Dhaka",
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        shipping_method: "NO",
+        product_name: "laptop",
+        product_category: "topup",
+        product_profile: "general",
+        multi_card_name: "mastercard,visacard,amexcard",
+        value_a: "ref001_A",
+        value_b: "ref002_B",
+        value_c: "ref003_C",
+        value_d: "ref004_D",
+      };
+      const response = await axios({
+        method: "POST",
+        url: "https://sandbox.sslcommerz.com/gwprocess/v4/api.php",
+        data: initiateData,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      });
+
+      const saveData = {
+        cus_name: paymentssl.name,
+        cus_email: paymentssl.email,
+        paymentId: trxId,
+        amount: paymentssl.amount,
+        status: "pending",
+        date: new Date(),
+        cartIds: paymentssl.cartIds,
+        menuItemIds: paymentssl.menuItemIds,
+      };
+
+      const save = await sslCollection.insertOne(saveData);
+      //   console.log("resSSL", response);
+
+      //   // delete cart items
+      //   const query = {
+      //     _id: {
+      //       $in: paymentssl.cartIds.map((id) => new ObjectId(id)),
+      //     },
+      //   };
+      //   const deleteResult = await cartCollection.deleteMany(query);
+
+      if (save) {
+        res.send({
+          paymentUrl: response.data.GatewayPageURL,
+        });
+      }
+    });
+
+    app.post("/success-payment", async (req, res) => {
+      const successData = req.body;
+      if (successData.status !== "VALID") {
+        throw new Error("Unauthorized payment, Invalid Payment");
+      }
+
+      // Update payment status
+      const query = {
+        paymentId: successData.tran_id,
+      };
+      const update = {
+        $set: {
+          status: "success",
+        },
+      };
+      const updateData = await sslCollection.updateOne(query, update);
+
+      // Delete cart items
+      const carts = await sslCollection.findOne(query);
+      if (carts) {
+        const deleteQuery = {
+          _id: { $in: carts.cartIds.map((id) => new ObjectId(id)) },
+        };
+        const deleteResult = await cartCollection.deleteMany(deleteQuery);
+        console.log("Deleted cart items:", deleteResult);
+      }
+
+      res.redirect("http://localhost:5173/dashboard/cart");
+    });
+
+    app.post("/fail", async (req, res) => {
+      res.redirect("http://localhost:5173/dashboard/cart");
+    });
+
+    app.post("/cancle", async (req, res) => {
+      res.redirect("http://localhost:5173/dashboard/cart");
+    });
+
+    app.get("/ssl", async (req, res) => {
+      const result = await sslCollection.find().toArray();
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
